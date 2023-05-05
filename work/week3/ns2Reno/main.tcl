8422c4efc2cc84aab20ef1cb91f0cc6192659f05
agsargsyan <agsargsyanos@gmail.com>
2023-05-05 16:40:14 +0300

    Added 3 week

---
+++ work/week3/ns2Reno/main.tcl
@@ -0,0 +1,34 @@
+#создаем симулятор
+set ns [new Simulator]
+#Открыть трейс-файл для nam
+set nf [open output/out.nam w]
+$ns namtrace-all $nf
+
+source "node.tcl"
+source "link.tcl"
+source "queue.tcl"
+source "connections.tcl"
+source "timing.tcl"
+source "nam.tcl"
+source "finish.tcl"
+
+$ns run
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
