ab7164e2d26470adc13b35ecad8313b67c3954ac
agsargsyan <agsargsyanos@gmail.com>
2023-06-11 11:02:02 +0300

    final report

--- work/report_last/auto/ch01.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "ch01"
- (lambda ()
-   (LaTeX-add-labels
-    "fig:1.1"
-    "fig:1.3"))
- :latex)
-
--- work/report_last/auto/ch02.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "ch02"
- (lambda ()
-   (LaTeX-add-labels
-    "fig:2.1"
-    "fig:2.4"))
- :latex)
-
